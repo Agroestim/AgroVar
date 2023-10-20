a6a87eba41556c8e03e9433727275e3c543e8b7a
Flangrys <63200230+Flangrys@users.noreply.github.com>
2023-10-20 08:38:32 -0300

    impl: Implementing resolver for variety_comparator

--- agrovar/schema.py
+++ agrovar/schema.py
@@ -3,13 +3,10 @@ import graphene_django
 
 from api import models
 
-# TODO: Definir los esquemas y mutaciones en base a los modelos y consultas
-# relevantes que se realizaran en la aplicacion.
-
 
 class VarietyPaperDataType(graphene_django.DjangoObjectType):
     """
-    ### GraphQL-Django objectType
+    ### Agrovar Internal
     Defines how `VarietyPaperModel` are represented in a GraphQL object type definition.
     """
 
@@ -20,7 +17,7 @@ class VarietyPaperDataType(graphene_django.DjangoObjectType):
 
 class TechnicalDataType(graphene_django.DjangoObjectType):
     """
-    ### GrapQL-Django ObjectType
+    ### Agrovar Internal
     Defines how `TechnicalDataModel` are represented in a GraphQL object type definition.
     """
 
@@ -29,23 +26,57 @@ class TechnicalDataType(graphene_django.DjangoObjectType):
         fields = "__all__"
 
 
+class LocationRankingType(graphene.ObjectType):
+    """
+    ### Agrovar GraphQL API
+    This type represents a ranking sorted by location where each variants are compared with each other according to their location.
+    """
+
+    class Meta:
+        description = "Consulta estatica para el ranking de variedades por localidad."
+
+
+class VarietyComparatorType(graphene.ObjectType):
+    """
+    ### Agrovar Graphql API
+    This type represents a list of varieties where each variant are compared with each other.
+    """
+
+    class Meta:
+        description = "Consulta estatica para el comparador de variedades."
+
+    campaing = graphene.Date()
+    location = graphene.String()
+    variety = graphene.String()
+
+
 class TechnicalDataQueryType(graphene.ObjectType):
     """
-    ### Graphene ObjectType
-    Represents a ranking
+    ### Agrovar Endpoint GraphQL API
+    This type represents the endpoint of GraphQL for performing queries in general.
+
     """
 
-    location_ranking = graphene.List(TechnicalDataType, location=graphene.String())
-    """Represents a list of varities ranked by location."""
+    class Meta:
+        description = "Representacion a la consulta de los analisis de las campañas y sus datos tecnicos."
+
+    location_ranking = graphene.List(
+        TechnicalDataType, location=graphene.String(required=True)
+    )
+
+    variety_comparator = graphene.List(
+        VarietyComparatorType, varieties=graphene.List(graphene.String)
+    )
 
     def resolve_location_ranking(self, info, location: str):
-        location = location.upper()
-
         related_ranking = models.TechnicalDataModel.objects.filter(
-            location__exact=location
+            location__exact=location.upper()
         ).select_related("paper_fk")
 
         return related_ranking
+
+    def resolve_variety_comparator(self, info, varieties: list[str]):
+        return models.TechnicalDataModel.objects.none()
 
 
 SCHEMA = graphene.Schema(query=TechnicalDataQueryType)
